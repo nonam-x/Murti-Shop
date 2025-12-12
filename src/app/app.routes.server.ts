d9f12535351e344e30fe41c2b49b63f02cdd50ea
nonam-x <cuet504@gmail.com>
2025-12-12 15:31:56 +0530

    app.router updated

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,20 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'Productgrid/:category',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      // Yahan tumhare saare categories list karo
+      return [
+        { category: 'all' },
+        { category: 'shirts' },
+        { category: 't-shirts' },
+        { category: 'hoodies' },
+        { category: 'jackets' },
+        { category: 'jeans' }
+      ];
+    }
   }
 ];
